9bfeac4721992f3349811e737ca1905523762f79
Mike Heitmann, N0SO <n0so@arrl.net>
2024-04-30 06:45:35 +0100

    First code commit

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+Update History:
+* Tue Apr 29 2024 Mike Heitmann, N0SO <n0so@arrl.net>
+- V0.0.1 - First interation
+
+"""
+VERSION = '0.0.1'
+DEFAULTDEVICE = '/dev/ttyACM0'
---
+++ epicmon.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from serial import Serial
+from epicmon.__init__ import VERSION
+from datetime import datetime
+
+class epicMon():
+    def __init__(self, devicename = None):
+        self.deviceName = devicename
+
+        if devicename:
+            self.serialCon = self.openDevice(devicename)
+        else:
+            self.serialCon = None
+
+    def openDevice(self,  serialport):
+        try:
+            ser = Serial(serialport)
+        except:
+            print('Error opening serial port {}'.format(serialport))
+            ser = None
+        return ser
+
+    def readPort(self):
+        return self.serialCon.readline().decode().rstrip()
+
+    def readBytes(self):
+        """Read and return byte arry"""
+        return self.serialCon.readline()
+
+    def getEpicVersion(self):
+        status = None
+        if self.serialCon:
+           status=list()
+           self.serialCon.write(b'\n')
+           for i in range(4):
+               st = self.readPort()
+               if i >  1:
+                   status.append(st)
+        return status
+
+    def epicMonTerm(self, loops=None):
+        self.serialCon.write(b'\n')
+        if loops != '0':
+            for i in range(int(loops)):
+               print(self.readPort())
+        else:
+            while 1:
+               print(self.readPort())
